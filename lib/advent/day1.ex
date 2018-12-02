3dec83771ed124d2cb3852fce5b088d3a91c64d8
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2018-12-02 21:46:15 +0100

    Day 1-2

--- lib/advent/day1.ex
+++ lib/advent/day1.ex
@@ -25,6 +25,32 @@ defmodule Advent.Day1 do
   +1, +1, -2 results in  0
   -1, -2, -3 results in -6
   Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
+
+  Your puzzle answer was 486.
+
+  --- Part Two ---
+  You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to find the first frequency it reaches twice.
+
+  For example, using the same list of changes above, the device would loop as follows:
+
+  Current frequency  0, change of +1; resulting frequency  1.
+  Current frequency  1, change of -2; resulting frequency -1.
+  Current frequency -1, change of +3; resulting frequency  2.
+  Current frequency  2, change of +1; resulting frequency  3.
+  (At this point, the device continues from the start of the list.)
+  Current frequency  3, change of +1; resulting frequency  4.
+  Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+  In this example, the first frequency reached twice is 2. Note that your device might need to repeat its list of frequency changes many times before a duplicate frequency is found, and that duplicates might be found while in the middle of processing the list.
+
+  Here are other examples:
+
+  +1, -1 first reaches 0 twice.
+  +3, +3, +4, -2, -4 first reaches 10 twice.
+  -6, +3, +8, +5, -6 first reaches 5 twice.
+  +7, +7, -2, -7, -4 first reaches 14 twice.
+  What is the first frequency your device reaches twice?
+
+  Your puzzle answer was 69285.
   """
 
   @doc """
@@ -34,8 +60,26 @@ defmodule Advent.Day1 do
   @spec final_frequency(String.t()) :: integer
   def final_frequency(input) do
     input
-    |> parse
+    |> parse()
     |> Enum.reduce(&Kernel.+/2)
+  end
+
+  @doc """
+  Returns the first duplicate frequency
+  """
+  def first_duplicate(input) do
+    input
+    |> parse()
+    |> Stream.cycle()
+    |> Enum.reduce_while({0, MapSet.new([0])}, fn freq, {acc, map} ->
+      new_acc = acc + freq
+
+      if MapSet.member?(map, new_acc) do
+        {:halt, new_acc}
+      else
+        {:cont, {new_acc, MapSet.put(map, new_acc)}}
+      end
+    end)
   end
 
   defp parse(input) do
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -3,24 +3,76 @@ defmodule Advent.Day1Test do
 
   alias Advent.Day1
 
-  test "example input" do
-    result =
-      """
-      +1
-      +1
-      +1
-      """
-      |> Day1.final_frequency()
+  describe "part 1" do
+    test "example input" do
+      result =
+        """
+        +1
+        +1
+        +1
+        """
+        |> Day1.final_frequency()
 
-    assert result == 3
+      assert result == 3
+    end
+
+    test "puzzle input" do
+      result =
+        "inputs/day1_1.txt"
+        |> File.read!()
+        |> Day1.final_frequency()
+
+      assert result == 486
+    end
   end
 
-  test "puzzle input" do
-    result =
-      "inputs/day1_1.txt"
-      |> File.read!()
-      |> Day1.final_frequency()
+  describe "part 2" do
+    test "example input 1" do
+      result =
+        """
+        +1
+        -1
+        """
+        |> Day1.first_duplicate()
 
-    assert result == 486
+      assert result == 0
+    end
+
+    test "example input 2" do
+      result =
+        """
+        +3
+        +3
+        +4
+        -2
+        -4
+        """
+        |> Day1.first_duplicate()
+
+      assert result == 10
+    end
+
+    test "example input 3" do
+      result =
+        """
+        +7
+        +7
+        -2
+        -7
+        -4
+        """
+        |> Day1.first_duplicate()
+
+      assert result == 14
+    end
+
+    test "puzzle input" do
+      result =
+        "inputs/day1_1.txt"
+        |> File.read!()
+        |> Day1.first_duplicate()
+
+      assert result == 69285
+    end
   end
 end
