0d83b5d0d18691835f7e260b0a6859f2fcabe41f
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2018-12-02 22:13:30 +0100

    Day 2-1

---
+++ test/day2_test.exs
@@ -0,0 +1,32 @@
+defmodule Advent.Day2Test do
+  use ExUnit.Case
+
+  alias Advent.Day2
+
+  describe "part 1" do
+    test "example input" do
+      result =
+        """
+        abcdef
+        bababc
+        abbcde
+        abcccd
+        aabcdd
+        abcdee
+        ababab
+        """
+        |> Day2.checksum()
+
+      assert result == 12
+    end
+
+    test "puzzle input" do
+      result =
+        "inputs/day2_1.txt"
+        |> File.read!()
+        |> Day2.checksum()
+
+      assert result == 7410
+    end
+  end
+end
