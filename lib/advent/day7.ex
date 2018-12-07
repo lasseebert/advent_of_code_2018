99e899426c9d321e6d4779afaac409e8021eabf3
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2018-12-07 14:18:17 +0100

    Day 7-2

--- lib/advent/day7.ex
+++ lib/advent/day7.ex
@@ -37,64 +37,158 @@ defmodule Advent.Day7 do
   So, in this example, the correct order is CABDFE.
 
   In what order should the steps in your instructions be completed?
+
+  --- Part Two ---
+  As you're about to begin construction, four of the Elves offer to help. "The sun will set soon; it'll go faster if we work together." Now, you need to account for multiple people working on steps simultaneously. If multiple steps are available, workers should still begin them in alphabetical order.
+
+  Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on. So, step A takes 60+1=61 seconds, while step Z takes 60+26=86 seconds. No time is required between steps.
+
+  To simplify things for the example, however, suppose you only have help from one Elf (a total of two workers) and that each step takes 60 fewer seconds (so that step A takes 1 second and step Z takes 26 seconds). Then, using the same instructions as above, this is how each second would be spent:
+
+  Second   Worker 1   Worker 2   Done
+   0        C          .        
+   1        C          .        
+   2        C          .        
+   3        A          F       C
+   4        B          F       CA
+   5        B          F       CA
+   6        D          F       CAB
+   7        D          F       CAB
+   8        D          F       CAB
+   9        D          .       CABF
+  10        E          .       CABFD
+  11        E          .       CABFD
+  12        E          .       CABFD
+  13        E          .       CABFD
+  14        E          .       CABFD
+  15        .          .       CABFDE
+  Each row represents one second of time. The Second column identifies how many seconds have passed as of the beginning of that second. Each worker column shows the step that worker is currently doing (or . if they are idle). The Done column shows completed steps.
+
+  Note that the order of the steps has changed; this is because steps now take time to finish and multiple workers can begin multiple steps simultaneously.
+
+  In this example, it would take 15 seconds for two workers to complete these steps.
+
+  With 5 workers and the 60+ second step durations described above, how long will it take to complete all of the steps?
   """
+
+  defmodule Graph do
+    defstruct [:ready, :blocks, :depends]
+
+    def new(block_list) do
+      # Map of %{node => [nodes]} meaning node blocks nodes
+      blocks = Enum.reduce(block_list, %{}, fn {a, b}, map -> Map.update(map, a, [b], fn list -> [b | list] end) end)
+
+      # Map of %{node => MapSet(nodes)} meaning node depends on nodes
+      depends =
+        block_list
+        |> Enum.reduce(%{}, fn {a, b}, map ->
+          Map.update(map, b, MapSet.new([a]), fn set -> MapSet.put(set, a) end)
+        end)
+
+      # List of nodes that does not depend on other nodes
+      ready =
+        block_list
+        |> Enum.flat_map(fn {a, b} -> [a, b] end)
+        |> Enum.uniq()
+        |> Enum.reject(&Map.has_key?(depends, &1))
+
+      %__MODULE__{ready: ready, blocks: blocks, depends: depends}
+    end
+
+    def step(%{ready: ready} = graph) do
+      first = ready |> Enum.sort() |> hd
+      graph = step(graph, first)
+      {graph, first}
+    end
+
+    def step(%{ready: ready, depends: depends, blocks: blocks} = graph, node) do
+      ready = List.delete(ready, node)
+
+      {ready, depends} =
+        blocks
+        |> Map.get(node, %{})
+        |> Enum.reduce({ready, depends}, fn dependent, {ready, depends} ->
+          blockers = depends |> Map.fetch!(dependent) |> MapSet.delete(node)
+
+          # Add to ready if no more blockers exist
+          ready = if MapSet.size(blockers) == 0, do: [dependent | ready], else: ready
+
+          # Update blockers
+          depends = Map.put(depends, dependent, blockers)
+
+          {ready, depends}
+        end)
+
+      %{graph | ready: ready, depends: depends}
+    end
+  end
 
   @doc "Part 1"
   @spec order(String.t()) :: String.t()
   def order(input) do
-    block_list = parse(input)
-
-    # Map of %{node => [nodes]} meaning node blocks nodes
-    blocks = Enum.reduce(block_list, %{}, fn {a, b}, map -> Map.update(map, a, [b], fn list -> [b | list] end) end)
-
-    # Map of %{node => MapSet(nodes)} meaning node depends on nodes
-    depends =
-      block_list
-      |> Enum.reduce(%{}, fn {a, b}, map ->
-        Map.update(map, b, MapSet.new([a]), fn set -> MapSet.put(set, a) end)
-      end)
-
-    # List of nodes that does not depend on other nodes
-    ready =
-      block_list
-      |> Enum.flat_map(fn {a, b} -> [a, b] end)
-      |> Enum.uniq()
-      |> Enum.reject(&Map.has_key?(depends, &1))
-
-    run(ready, blocks, depends, "")
+    input
+    |> parse()
+    |> run("")
   end
 
-  defp run([], _blocks, _depends, acc) do
+  defp run(%{ready: []}, acc) do
     acc
   end
 
-  defp run(ready, blocks, depends, acc) do
-    [next | ready] = ready |> Enum.sort()
-    acc = acc <> next
-
-    {ready, depends} =
-      blocks
-      |> Map.get(next, %{})
-      |> Enum.reduce({ready, depends}, fn dependent, {ready, depends} ->
-        blockers = depends |> Map.fetch!(dependent) |> MapSet.delete(next)
-
-        # Add to ready if no more blockers exist
-        ready = if MapSet.size(blockers) == 0, do: [dependent | ready], else: ready
-
-        # Update blockers
-        depends = Map.put(depends, dependent, blockers)
-
-        {ready, depends}
-      end)
-
-    run(ready, blocks, depends, acc)
+  defp run(graph, acc) do
+    {graph, node} = Graph.step(graph)
+    acc = acc <> node
+    run(graph, acc)
   end
 
-  # Returns {a, b} which means "a blocks b" or "b depends on a"
+  @doc "Part 2"
+  @spec time(String.t(), integer, integer) :: integer
+  def time(input, num_workers, added_time) do
+    graph = parse(input)
+    workers = []
+    time = -1
+
+    run_time(graph, workers, num_workers, added_time, time)
+  end
+
+  defp run_time(%{ready: []}, [], _, _, time) do
+    time
+  end
+
+  defp run_time(graph, workers, num_workers, added_time, time) do
+    workers = workers |> Enum.map(fn {node, time} -> {node, time - 1} end)
+    {done_workers, workers} = Enum.split_with(workers, fn {_node, time} -> time == 0 end)
+    graph = Enum.reduce(done_workers, graph, fn {node, _}, graph -> Graph.step(graph, node) end)
+
+    workers = put_to_work(graph, workers, num_workers, added_time)
+    run_time(graph, workers, num_workers, added_time, time + 1)
+  end
+
+  defp put_to_work(graph, workers, num_workers, added_time) do
+    cond do
+      length(workers) == num_workers ->
+        workers
+
+      length(graph.ready) == length(workers) ->
+        workers
+
+      true ->
+        ongoing = workers |> Enum.map(&elem(&1, 0))
+        node = graph.ready |> Enum.find(fn node -> node not in ongoing end)
+        put_to_work(graph, [{node, work_time(node) + added_time} | workers], num_workers, added_time)
+    end
+  end
+
+  defp work_time(node) do
+    <<ascii>> = node
+    ascii - ?A + 1
+  end
+
   defp parse(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&parse_line/1)
+    |> Graph.new()
   end
 
   defp parse_line(line) do
--- test/day7_test.exs
+++ test/day7_test.exs
@@ -23,4 +23,25 @@ defmodule Advent.Day7Test do
       assert Day7.order(input) == "JRHSBCKUTVWDQAIGYOPXMFNZEL"
     end
   end
+
+  describe "part 2" do
+    test "example input" do
+      input = """
+      Step C must be finished before step A can begin.
+      Step C must be finished before step F can begin.
+      Step A must be finished before step B can begin.
+      Step A must be finished before step D can begin.
+      Step B must be finished before step E can begin.
+      Step D must be finished before step E can begin.
+      Step F must be finished before step E can begin.
+      """
+
+      assert Day7.time(input, 2, 0) == 15
+    end
+
+    test "puzzle input" do
+      input = File.read!("inputs/day7_1.txt")
+      assert Day7.time(input, 5, 60) == 975
+    end
+  end
 end
